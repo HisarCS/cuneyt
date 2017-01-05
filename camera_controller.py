e215d7c6433a77616e575e628d418188900fe98c
cem <cersozz@gmail.com>
2017-01-05 06:14:07 -0500

    handling exceptions by printingin the camera_controller module

--- camera_controller.py
+++ camera_controller.py
@@ -1,16 +1,28 @@
 import picamera
 class camera_controller:
 
-    def __init__(self, pwm):
+    '''
+        all functions for camera_controller handle exceptions internally,
+        printing them. (Will create error log in the future)
+        all functions return True if no exceptions occured, False if they
+        could not run to completion
+    '''
+    def __init__(self, pwm, flip):
         #initialize the raspberry pi camera module
         self.camera = picamera.PiCamera()
+        camera.resolution = (1024, 768)
+            
         #update this
         self.servo_pin = 0
         #add pwm controller as a field
         self.pwm = pwm
         #set recording to false
-        self.recording = false
-    
+        self.recording = False
+        #set previewing to false
+        self.previewing = False
+        #flip the camera vertically, if flip = True
+        self.camera.vflip = flip
+
     ''' function look_at:
         sets servo angle to the given angle
         note that for our servo and pwn shield, the scaling -angle -> pwm-
@@ -21,38 +33,122 @@ class camera_controller:
 
     ''' function take_picture:
         wrapper for camera.capture, saves image with the given name
+        returns a boolean, True if capture was successful, false if not
+        handles errors internally
     '''
+    #TODO: create error log instead of prints
     def take_picture(self, name):
-        self.camera.capture('./captures' + name)
+        try:
+            self.camera.capture('./captures' + name)
+            return True
+        except PiCameraError as err:
+            print ("Camera error: " + err)
+            return False
 
     ''' function take_picture_at:
         sets servo angle to the given angle and takes a picture
         wrapper for look_at AND take_picture
     '''
     def take_picture_at(self, angle, name):
-        self.look_at(angle)
-        self.take_picture(name)
+        self.look_at(angle) and self.take_picture(name)
+        
 
     ''' function start_video_recording:
         wrapper for camera.start_recording()
         starts saving the video with the given name
     '''
     def start_video_recording(self, name):
-        self.camera.start_recording('./videos' + name)
-        self.recording = True
+        if self.recording:
+            print ("Tried to start an existing process: video recording")
+            return False
+        else:
+            try:
+                self.camera.start_recording('./videos' + name)
+                self.recording = True
+                return True
+            except PiCameraError as err:
+                print("Camera error: " + err)
+                return False
 
     ''' function stop_video_recording:
         wrapper for camera.stop_recording()
-        stops video recording
+        stops video recording, if exists
     '''
     def stop_video_recording(self):
         if (not self.recording):
-            raise Exception("Camera not currently recording video")
+            print ("Tried to stop a non-existent process: video recording")
+            return False
         else:
-            self.camera.stop_recording()
+            try: 
+                self.camera.stop_recording()
+                return True
+            except PiCameraError as err:
+                print ("Camera error: " + err)
+                return False
 
+    ''' function start_camera_preview:
+        wrapper for camera.start_preview()
+        starts a camera preview on the screen
+    '''
+    def start_camera_preview(self):
+        if (self.previewing):
+            print ("Tried to start an existing process: camera preview")
+            return False
+        else:
+            try:
+                self.camera.start_preview()
+                self.previewing = True
+                return True
+            except PiCameraError as err:
+                print ("Camera error: " + err)
+                return False
+        
+    
+    ''' function stop_camera_preview:
+        wrapper for camera.stop_preview()
+        stops the camera preview on the screen
+    '''
+    def stop_camera_preview(self):
+        if (not self.previewing):
+            print ("Tried to stop a non-existent process: camera preview")
+            return False
+        else:
+            try:
+                self.camera.stop_preview()
+                self.previewing = False
+                return True
+        except PiCameraError as err:
+            print ("Camera error: " + err)
+            return False
+   
+    ''' function start_camera_stream:
+        starts a camera stream, as a background process
+        streams the entire camera feed to the local network with the ip
+        address of the raspberry pi
+    '''
+    def start_camera_stream(self):
+        raise Exception("Not Yet Implemented")
+
+    ''' function stop_camera_stream:
+        stops the camera stream, if on
+        raises an Exception if camera stream is not running
+    '''
+    def stop_camera_stream(self):
+        raise Exception("Not Yet Implemented")
+
+    ''' function capture_vertical_panorama
+        captures multiple images at different vertical angles
+        with the given interval and stitches them together, in the background
+        to create a vertical panoramic image
+        interval is an integer i, such that 0 < i < 180
+    '''
     def capture_vertical_panorama(self, interval):
         raise Exception("Not Yet Implemented")
 
+    ''' function capture_vertical_panorama_at:
+        captures multiple images at the specified angles,
+        angles is an array of integers a, where for all i in a,
+        0 < i < 180 and no two members of the array are equal
+    '''
     def capture_vertical_panorama_at(self, angles):
         raise Exception("Not Yet Implemented")
