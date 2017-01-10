7c39ccb4f1d70b88ed4c2c84f607f9725ccfba1f
cem <cersozz@gmail.com>
2017-01-10 15:59:18 -0500

    added python kill to update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+echo "stopping cuneyt main execution"
+
+kill $(pgrep -f 'python main.py')
+
 echo "checking to update files"
+
 git pull
-echo "starting cuneyt script"
+
+echo "restarting cuneyt script"
+
 python main.py
+
 echo "done"
