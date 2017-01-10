3e1ab8a97576193ae8b77699e31bd22e7c57bbbd
Cem Ersoz <cersozz@gmail.com>
2017-01-10 16:12:39 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+'''
+This is a config file that Cuneyt needs to function,
+all following fields MUST be included for cuneyt to work
+actual values are not included due to security concerns
+'''
+mongo_user = "cuneyt"
+mongo_pass = "my_mongo_password"
+mongo_url = "my_mongo_url/my_mongo_db"
+log_file = "logger.txt" #these can be any filenames you want
+backlog_file = "backlog.csv"
