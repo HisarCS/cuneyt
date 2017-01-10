01dc109a6dfbe010cd057d50f2815141b19aaba9
cem <cersozz@gmail.com>
2017-01-10 16:20:20 -0500

    created placeholder main

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-#CUNEYT LIBRARY IMPORTS
-import motor_controller
-import lidar
-
-#STANDARD LIBRARY IMPORTS
-import RPi.GPIO as GPIO
+import cuneyt
 import time
-from multiprocessing import Process
-
-motors = motor_controller(GPIO)
-lidar = lidar(GPIO,time)
-time.sleep(2)
-lidar_process = Process(target = lidar.sweep)
-
+while True:
+    #do things here
+    time.sleep(0.5)
